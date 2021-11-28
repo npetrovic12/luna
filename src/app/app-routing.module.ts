73759a27d483f93563e3e0eccb9bdcad9bc6a6de
unknown <nikola20petrovic@gmail.com>
2021-11-28 10:19:20 +0100

    feat: Initial home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', loadChildren: () => import('./entities/home-page/home-page.module').then(m => m.HomePageModule) },
   { path: 'la-sposa', loadChildren: () => import('./entitites/creator-pages/la-sposa/la-sposa.module').then(m => m.LaSposaModule) },
 ];
 
