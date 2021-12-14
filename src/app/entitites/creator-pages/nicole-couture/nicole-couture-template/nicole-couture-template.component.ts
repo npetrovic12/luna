8487139f55b261cb8ee0145a59faf530cd2c3b99
MarkoMandic9 <marko.mandic4444@gmail.com>
2021-12-14 13:08:17 +0100

    Add template implementation

---
+++ src/app/entitites/creator-pages/nicole-couture/nicole-couture-template/nicole-couture-template.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nicole-couture-template',
+  templateUrl: './nicole-couture-template.component.html',
+  styleUrls: ['./nicole-couture-template.component.scss']
+})
+export class NicoleCoutureTemplateComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/entitites/creator-pages/nicole-couture/nicole-couture.module.ts
+++ src/app/entitites/creator-pages/nicole-couture/nicole-couture.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { NicoleCoutureRoutingModule } from './nicole-couture-routing.module';
 import { NicoleCoutureComponent } from './nicole-couture.component';
+import { NicoleCoutureTemplateComponent } from './nicole-couture-template/nicole-couture-template.component';
 
 
 @NgModule({
   declarations: [
     NicoleCoutureComponent,
+    NicoleCoutureTemplateComponent,
   ],
   imports: [
     CommonModule,
