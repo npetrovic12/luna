43ea53d5cc1a12f6c2dc7753efa2cc3956b70590
MarkoMandic9 <marko.mandic4444@gmail.com>
2022-02-06 21:26:36 +0100

    Update Nicole couture

--- src/app/entitites/creator-pages/nicole-couture/nicole-couture.module.ts
+++ src/app/entitites/creator-pages/nicole-couture/nicole-couture.module.ts
@@ -3,8 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { PageNavbarModule } from 'src/app/shared/components/page-navbar/page-navbar.module';
 import { NicoleCoutureTemplateComponent } from './nicole-couture-template/nicole-couture-template.component';
-import { NicoleCoutureRoutingModule } from './nicole-couture-routing.module';
 import { NicoleCoutureComponent } from './nicole-couture.component';
+import { ContentCardModule } from 'src/app/shared/components/content-card/content-card.module';
+import { HorizontalImageSliderModule } from 'src/app/shared/components/horizontal-image-slider/horizontal-image-slider.module';
+import { PageFooterModule } from 'src/app/shared/components/page-footer/page-footer.module';
+import { PhoneRibbonModule } from 'src/app/shared/components/phone-ribbon/phone-ribbon.module';
+import { DirectiveModule } from 'src/app/shared/directives/directive.module';
+import { NicoleCoutureRoutingModule } from './nicole-couture-routing.module';
 
 
 @NgModule({
@@ -15,7 +20,12 @@ import { NicoleCoutureComponent } from './nicole-couture.component';
   imports: [
     CommonModule,
     PageNavbarModule,
-    NicoleCoutureRoutingModule
+    NicoleCoutureRoutingModule,
+    PhoneRibbonModule,
+    HorizontalImageSliderModule,
+    PageFooterModule,
+    ContentCardModule,
+    DirectiveModule,
   ]
 })
 export class NicoleCoutureModule { }
