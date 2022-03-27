1a28ee9b31922c0a968ffeb6718d9071f3fda4e6
MarkoMandic9 <marko.mandic4444@gmail.com>
2022-03-27 18:33:13 +0200

    Text

--- src/app/entitites/creator-pages/st-patrick/content-cards/st-patrick-content-cards.model.ts
+++ src/app/entitites/creator-pages/st-patrick/content-cards/st-patrick-content-cards.model.ts
@@ -4,7 +4,7 @@ export const contentCards: ContentCard[] = [
   {
     imgPath: '/assets/images/st-patrick/sp4.webp',
     title: 'St. Patrick',
-    text: 'Zakoračite u magični svet St. Patrick-a i otkrijte venčanice koje krase klasična elegancija, luksuzne tkanine, božanski detalji od čipke i bezvremenske siluete neveste. ',
+    text: 'Zakoračite u magični svet St. Patrick-a i otkrijte venčanice koje krase klasična elegancija, luksuzne tkanine, božanski detalji od čipke i bezvremenske siluete mlade. ',
   },
   {
     imgPath: '/assets/images/st-patrick/sp9.webp',
