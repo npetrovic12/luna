2221fa726951b78e715c06e321b728a5cf80834c
MarkoMandic9 <marko.mandic4444@gmail.com>
2022-04-15 19:40:35 +0200

    Compress images

--- src/app/entities/home-page/model/content-card.model.ts
+++ src/app/entities/home-page/model/content-card.model.ts
@@ -6,12 +6,12 @@ export interface ContentCard {
 
 export const mockContentCards: ContentCard[] = [
   {
-    imgPath: '/assets/images/home-page/m1.jpg',
+    imgPath: '/assets/images/home-page/m1.webp',
     title: 'La couture',
     text: 'Vreme je da stvorite magiju, zapocnite je kod nas. Zakoracite u svet vase najdivnije bajke jer snovi postaju stvarnost kada posetite nas salon venčanica. Usluga u našem salonu je presonalizovana, sto znaci da svaki klijent dobija potpunu posvecenost pri odabiru svoje vencanice. Nase vencanice ce vam omoguciti da upotpunite svoj nezaboravan dan, jer svaka od njih nosi poseban pecat da ćete prisutne goste ostaviti bez daha',
   },
   {
-    imgPath: '/assets/images/home-page/m3.jpg',
+    imgPath: '/assets/images/home-page/m3.webp',
     title: 'La couture',
     text: 'Probe u nasem salonu se zakazuju zbog kvalitetnije usluge i predanosti svakom nasem klijentu posebno. Mi ćemo se potruditi da se u nasem prostoru osecate prijatno jer sledece poglavlje vaseg zivota pocinje upravo kod nas. Kvalitetni materijali i jedinstven dizajn su ono sto opisuje nase vencanice.',
   },
