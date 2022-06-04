01115b6117609c59ddf8a3e445d363ea66228bc9
nebojsa <nebojsa.zakic@synsoft.com>
2022-06-04 11:34:24 +0200

    Update

--- src/app/entities/home-page/model/content-card.model.ts
+++ src/app/entities/home-page/model/content-card.model.ts
@@ -3,6 +3,7 @@ export interface ContentCard {
   title: string;
   alt: string;
   text: string;
+  defaultImageHeight?: number;
 }
 
 export const mockContentCards: ContentCard[] = [
@@ -11,11 +12,13 @@ export const mockContentCards: ContentCard[] = [
     alt: 'La Couture Venčanica',
     title: 'La couture - Studio Luna',
     text: 'Vreme je da stvorite magiju, zapocnite je kod nas. Zakoracite u svet vase najdivnije bajke jer snovi postaju stvarnost kada posetite nas salon venčanica. Usluga u našem salonu je presonalizovana, sto znaci da svaki klijent dobija potpunu posvecenost pri odabiru svoje vencanice. Nase vencanice ce vam omoguciti da upotpunite svoj nezaboravan dan, jer svaka od njih nosi poseban pecat da ćete prisutne goste ostaviti bez daha',
+    defaultImageHeight: 450,
   },
   {
     imgPath: '/assets/images/home-page/m3.webp',
     title: 'La couture',
     alt: 'La Couture Venčanica - Studio Luna',
     text: 'Probe u nasem salonu se zakazuju zbog kvalitetnije usluge i predanosti svakom nasem klijentu posebno. Mi ćemo se potruditi da se u nasem prostoru osecate prijatno jer sledece poglavlje vaseg zivota pocinje upravo kod nas. Kvalitetni materijali i jedinstven dizajn su ono sto opisuje nase vencanice.',
+    defaultImageHeight: 450,
   },
 ];
