a37e2ee165f5917e362edc103ce8569046c3113e
nebojsa <nebojsa.zakic@synsoft.com>
2022-06-04 10:26:27 +0200

    Css improvements

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,9 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./entities/home-page/home-page.module').then(m => m.HomePageModule) },
-  { path: 'la-sposa', loadChildren: () => import('./entitites/creator-pages/la-sposa/la-sposa.module').then(m => m.LaSposaModule) },
-  { path: 'nicole-couture', loadChildren: () => import('./entitites/creator-pages/nicole-couture/nicole-couture.module').then(m => m.NicoleCoutureModule) },
-  { path: 'st-patrick', loadChildren: () => import('./entitites/creator-pages/st-patrick/st-patrick.module').then(m => m.StPatrickModule) },
+  { path: 'la-sposa', loadChildren: () => import('./entities/la-sposa/la-sposa.module').then(m => m.LaSposaModule) },
+  { path: 'nicole-couture', loadChildren: () => import('./entities/nicole-couture/nicole-couture.module').then(m => m.NicoleCoutureModule) },
+  { path: 'st-patrick', loadChildren: () => import('./entities/st-patrick/st-patrick.module').then(m => m.StPatrickModule) },
   { path: 'contact', loadChildren: () => import('./entities/contact-page/contact-page.module').then(m => m.ContactPageModule) },
   { path: 'about', loadChildren: () => import('./entities/about/about.module').then(m => m.AboutModule) }
 ];
--- src/app/entities/home-page/components/card-group/card-group.component.ts
+++ src/app/entities/home-page/components/card-group/card-group.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, OnInit } from '@angular/core';
 import { CreatorCard } from '../../model/creator-card.model';
 
 @Component({
@@ -6,6 +6,17 @@ import { CreatorCard } from '../../model/creator-card.model';
   templateUrl: './card-group.component.html',
   styleUrls: ['./card-group.component.scss']
 })
-export class CardGroupComponent {
+export class CardGroupComponent implements OnInit {
   @Input() creatorCards!: CreatorCard[];
+
+  constructor(private elemRef: ElementRef) {
+  }
+
+  ngOnInit(): void {
+    if (window.innerWidth >= 600) return;
+    setInterval(() => {
+      const scrollPos = Math.ceil(((this.elemRef.nativeElement.scrollLeft / (this.creatorCards?.length ?? 1)) % window.outerWidth + 1) % this.creatorCards?.length);
+      this.elemRef.nativeElement.scrollLeft = scrollPos * window.outerWidth + 16;
+    }, 5000);
+  }
 }
